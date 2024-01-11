0fb5ed9fc666f414692f09a6399acf22b4dadbf8
Sharon-SHH <sharonheng.shi@gmail.com>
2024-01-11 13:32:11 -0500

    add gifs

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-// import axios from "./libs/axios";
-
 const root = document.querySelector(".gifs__details");
 
 // Get data from GifAPI
@@ -11,34 +9,41 @@ const categoriesURL =
 
 
 
- async function fetch() {
+function addToHTML(gifs) {
+  // const gifs = gifsList.data[3].gif;
+  // create an image element
+  // const img = document.createElement("img");
+  // img.src = gifs.images["480w_still"].url;
+
+  const img1 = document.createElement("img");
+  img1.src = gifs.images["downsized"].url;
+
+  // const mp4 = document.createElement("video");
+  // const source = document.createElement("source");
+  // source.src = gifs.images["downsized_small"].mp4;
+  // source.type = "video/mp4";
+  // mp4.autoplay = true;
+  // mp4.appendChild(source);
+  // console.log(source);
+  // console.log(mp4);
+
+  // root.appendChild(img);
+  root.appendChild(img1);
+  // root.appendChild(mp4);
+}
+ async function fetchAPI() {
     try {
         let url = categoriesURL + KEY;
         console.log(url);
         const response = await axios.get(categoriesURL+KEY);
-        const gifs = response.data.data[3].gif;
-        console.log(gifs);
-        // create an image element
-        const img = document.createElement("img");
-        img.src = gifs.images["480w_still"].url;
-
-        const img1 = document.createElement("img");
-        img1.src = gifs.images["downsized"].url;
-
-        const mp4 = document.createElement("video");
-        const source = document.createElement("source");
-        source.src = gifs.images["downsized_small"].mp4;
-        source.type = "video/mp4";
-        mp4.autoplay = true;
-        mp4.appendChild(source);
-        console.log(source);
-        console.log(mp4);
-
-        root.appendChild(img);
-        root.appendChild(img1);
-        root.appendChild(mp4);
+        const gifsList = response.data.data;
+        console.log(gifsList);
+        gifsList.forEach((element) => {
+          addToHTML(element.gif)
+        });
+  
     } catch(error) {
         console.log(error);
     }
  } 
- fetch();
+fetchAPI();
